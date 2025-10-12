f9ef8bb2865652ad488427c4c0f352e25e67ed65
fujianmian <144903122+fujianmian@users.noreply.github.com>
2025-10-12 08:49:42 +0800

    fix

--- src/app/components/PDFViewer.tsx
+++ src/app/components/PDFViewer.tsx
@@ -1,11 +1,14 @@
 'use client';
 import React, { useState, useEffect, useMemo } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+// import 'react-pdf/dist/Page/AnnotationLayer.css';
+// import 'react-pdf/dist/Page/TextLayer.css';
 
 // Configure PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 interface PDFViewerProps {
   pdfUrl: string;
